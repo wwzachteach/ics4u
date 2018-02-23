c3aa8b10b301c0d83c06d69370bd440ee9c6e481
wwzachteach <36044243+wwzachteach@users.noreply.github.com>
2018-02-23 10:28:20 -0500

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,9 @@
+//
+// Interface
+// Created by Zachary Lawrence on 2018-02-23.
+
+public interface Math {
+	
+	double complex (double x, double y, double z); //Empty method, 3 variable equation
+	double simple (double x, double y); //Empty method, 2 variable equation
+}
